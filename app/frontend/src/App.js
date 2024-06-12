94e0ccc826a6afc9dabf0eb3a7fac3b489cb1e2f
guy-turo <kasongokakumbiguy@gmail.com>
2024-06-12 15:39:28 +0200

    3 commits

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react'
-import {Routes,Route}from 'react-router-dom'
+import React,{useState, useEffect} from 'react'
+import {Routes,Route} from 'react-router-dom'
 import LoginPage from './pages/auth/LoginPage';
 import RecoverPage from './pages/auth/RecoverPage';
 import RegisterPage from './pages/auth/RegisterPage';
@@ -8,9 +8,21 @@ import NotFound from './pages/NotFound';
 import SuperAdmin from './pages/admin/SuperAdmin';
 import Admin from './pages/admin/Admin';
 import ProfilePage from './pages/auth/ProfilePage';
+import PrivateRoute from './utils/helper/PrivateRoute';
 
 
 const  App=()=> {
+  const session= localStorage.getItem("sessionId")
+  const [isAuthenticate,setIsAuthenticate]=useState(session)
+
+  useEffect(()=>{
+    const session= localStorage.getItem("sessionId")
+    if(session && session!==''){
+      console.log("sessonId :",session)
+      setIsAuthenticate(true)
+    }
+  },[])
+  console.log('isAuthenticate :', isAuthenticate)
   return (
    <main>
     <Routes>
@@ -20,9 +32,9 @@ const  App=()=> {
       <Route path='/recover' element={<RecoverPage/>}/>
       <Route path='*' element={<NotFound/>}/>
 
-      <Route path='/superAdmin' element={<SuperAdmin/>}/>
-      <Route path='/admin' element={<Admin/>}/>
-      <Route path='/Profile' element={<ProfilePage/>}/>
+      <Route path='/superAdmin' element={<PrivateRoute isAuthenticated={isAuthenticate}><SuperAdmin/></PrivateRoute>}/>
+      <Route path='/admin' element={<PrivateRoute isAuthenticated={isAuthenticate}><Admin/></PrivateRoute>}/>
+      <Route path='/Profile' element={<PrivateRoute isAuthenticated={isAuthenticate}><ProfilePage/></PrivateRoute>}/>
     </Routes>
    </main>
   );
--- app/frontend/src/redux_tool.js/services/store.js
+++ app/frontend/src/redux_tool.js/services/store.js
@@ -6,7 +6,9 @@ export const store = configureStore({
     reducer: {
         [authApi.reducerPath]: authApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware)
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware()
+        .concat(authApi.middleware)
 })
 
 setupListeners(store.dispatch)
